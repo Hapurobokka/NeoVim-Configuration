0ccec63185385117b6d4133d92cf64944cc1d4d0
Hapurobokka <flaco.legos@gmail.com>
2024-04-13 12:18:35 -0600

    acomodando configuracion

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -358,33 +358,28 @@ local default_plugins = {
             workspaces = {
                 {
                     name = "vault",
-                    path = "C:\\Users\\cosmo\\OneDrive\\Documents\\vault",
-                },
-                {
-                    name = "aire",
-                    path = "C:\\Users\\cosmo\\OneDrive\\Documents\\aire",
-                    overrides = {
-                        templates = {
-                            subdir = "\\Plantillas"
-                        }
+                    path = "$HOME/aire/",
+                    templates = {
+                        subdir = "/Plantillas"
                     }
                 },
-                -- {
-                --     name = "no-vault",
-                --     path = function()
-                --     -- alternatively use the CWD:
-                --     -- return assert(vim.fn.getcwd())
-                --         return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
-                --     end,
-                --     overrides = {
-                --         notes_subdir = vim.NIL,  -- have to use 'vim.NIL' instead of 'nil'
-                --         new_notes_location = "current_dir",
-                --         templates = {
-                --             subdir = vim.NIL,
-                --         },
-                --     disable_frontmatter = true,
-                --     },
-                -- },
+
+                {
+                    name = "no-vault",
+                    path = function()
+                    -- alternatively use the CWD:
+                    -- return assert(vim.fn.getcwd())
+                        return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+                    end,
+                    overrides = {
+                        notes_subdir = vim.NIL,  -- have to use 'vim.NIL' instead of 'nil'
+                        new_notes_location = "current_dir",
+                        templates = {
+                            subdir = vim.NIL,
+                        },
+                    disable_frontmatter = true,
+                    },
+                },
             },
             -- Optional, customize how note IDs are generated given an optional title.
             ---@param title string|?
@@ -429,7 +424,7 @@ local default_plugins = {
         ft = { "markdown" },
         build = function() vim.fn["mkdp#util#install"]() end,
     },
-    
+
     {
         "preservim/vim-pencil",
         cmd = { "Pencil", "NoPencil", "TooglePencil", "SoftPencil", "HardPencil" }
