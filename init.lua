bc38f362872d412b63098872c073700d6e414e4f
Hapurobokka <flaco.legos@gmail.com>
2024-04-30 23:42:17 -0600

    La movi a algo

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require "core"
+require "custom.configs.hydras"
 
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
@@ -20,36 +21,63 @@ dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
 
-require("nvim-treesitter.configs").setup({
+require("nvim-treesitter.configs").setup {
     ensure_installed = { "markdown", "markdown_inline", ... },
     highlight = {
         enable = true,
     },
-})
+}
 
-require('telescope').load_extension('media_files')
-require'telescope'.setup {
-  extensions = {
-    media_files = {
-      -- filetypes whitelist
-      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-      filetypes = {"png", "webp", "jpg", "jpeg"},
-      -- find command (defaults to `fd`)
-      find_cmd = "rg"
-    }
-  },
+require("telescope").load_extension "media_files"
+require("telescope").setup {
+    extensions = {
+        media_files = {
+            -- filetypes whitelist
+            -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+            filetypes = { "png", "webp", "jpg", "jpeg" },
+            -- find command (defaults to `fd`)
+            find_cmd = "rg",
+        },
+    },
 }
 
 require("oil").setup()
 
-vim.cmd("set spelllang=es")
+vim.cmd "set spelllang=es"
 
-local lspconfig = require('lspconfig')
+local lspconfig = require "lspconfig"
 lspconfig.tsserver.setup {}
-lspconfig.pylsp.setup{}
-
+lspconfig.pylsp.setup {}
 
 if vim.g.neovide then
     vim.o.guifont = "JetBrainsMono Nerd Font:h12"
     -- Aquí pasan cosas de NeoVide
 end
+
+vim.cmd "KanagawaCompile"
+
+local rocks_config = {
+    rocks_path = vim.env.HOME .. "/.local/share/nvim/rocks",
+    luarocks_binary = vim.env.HOME .. "/.local/share/nvim/rocks/bin/luarocks",
+}
+
+vim.g.rocks_nvim = rocks_config
+
+local luarocks_path = {
+    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+}
+package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+
+local luarocks_cpath = {
+    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+    -- Remove the dylib and dll paths if you do not need macos or windows support
+    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.dylib"),
+    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.dylib"),
+    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.dll"),
+    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.dll"),
+}
+package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+
+vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,15 +1,16 @@
 local opt = vim.opt
 local g = vim.g
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
 --
 --
 --------------------------options-------------------------
 opt.wrap = true
 opt.linebreak = true
+opt.foldlevel = 1
 
