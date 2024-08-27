ca6b090fbf0db4058e30ce6a12757d9fcf19eb4e
Hapurobokka <flaco.legos@gmail.com>
2024-08-26 20:05:07 -0600

    updating config Mon Aug 26 08:05:07 PM CST 2024

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -318,25 +318,6 @@ local plugins = {
     {
         "Olical/conjure",
         ft = { "clojure", "fennel", "python", "lisp" }, -- etc
-        -- [Optional] cmp-conjure for cmp
-        dependencies = {
-            {
-                "PaterJason/cmp-conjure",
-                config = function()
-                    local cmp = require "cmp"
-                    local config = cmp.get_config()
-                    table.insert(config.sources, {
-                        name = "buffer",
-                        option = {
-                            sources = {
-                                { name = "conjure" },
-                            },
-                        },
-                    })
-                    cmp.setup(config)
-                end,
-            },
-        },
         config = function(_, opts)
             require("conjure.main").main()
             require("conjure.mapping")["on-filetype"]()
