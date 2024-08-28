d816f25f61b8e4508fcaf8bff3d7ad3defc088c5
Hapurobokka <flaco.legos@gmail.com>
2024-08-28 13:56:00 -0600

    updating config Wed Aug 28 01:56:00 PM CST 2024

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,13 @@
-function fish_prompt
-    set -l last_status $status
-
-    set -l stat 
-    if test $last_status -ne 0
-        set stat (set_color red)"[$last_status]"(set_color normal)
-    end
-
-    string join '' -- (set_color green) (prompt_pwd --full-length-dirs 2) (set_color normal) $stat '>' ' ' 
-end
+# function fish_prompt
+#     set -l last_status $status
+#
+#     set -l stat 
+#     if test $last_status -ne 0
+#         set stat (set_color red)"[$last_status]"(set_color normal)
+#     end
+#
+#     string join '' -- (set_color green) (prompt_pwd --full-length-dirs 2) (set_color normal) $stat '>' ' ' 
+# end
 
 abbr -a cfs chafa -f symbols
 
