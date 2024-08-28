4a9e65af12c5dcb026356c69e15e2988b3e703c5
Hapurobokka <flaco.legos@gmail.com>
2024-08-28 13:54:50 -0600

    updating config Wed Aug 28 01:54:50 PM CST 2024

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,16 @@
+function fish_prompt
+    set -l last_status $status
+
+    set -l stat 
+    if test $last_status -ne 0
+        set stat (set_color red)"[$last_status]"(set_color normal)
+    end
+
+    string join '' -- (set_color green) (prompt_pwd --full-length-dirs 2) (set_color normal) $stat '>' ' ' 
+end
+
 abbr -a cfs chafa -f symbols
+
 alias sys-rebuild="sudo nixos-rebuild switch --flake ~/nix-configs/#myNixos"
 alias home-rebuild="home-manager switch --flake ~/nix-configs/#hapu@nixos"
 
